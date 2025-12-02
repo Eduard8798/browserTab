6a16d10a89734dc659c0dd8ad053f2e8dc18295c
Eduard8798 <eduard9999997@gmail.com>
2025-12-02 20:54:56 +0200

    add

--- web-project/src/components/tabs/Tabs.jsx
+++ web-project/src/components/tabs/Tabs.jsx
@@ -77,7 +77,7 @@ const Tabs = () => {
             )
         );
     };
-    //add y
+    //add
 
     const dragStartHandler = (e, value) => setCurrentCard(value);
     const dragLeaveHandler = () => {};
